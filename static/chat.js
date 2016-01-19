9cbb6a7755568f31a5a225eea0dc416ae111af20
Igor Hatarist <igor@hatari.st>
2016-01-19 09:36:24 +0300

    Major rework - basic command/API scalability, better UI, got rid of the cached messages history for a while.

--- static/chat.js
+++ static/chat.js
@@ -1,76 +1,178 @@
-// Copyright 2009 FriendFeed
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License. You may obtain
-// a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
+function render_date() {
+    var date = new Date();
+    var formattedDate = '[' + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + '] ';
+    return $("<span>").addClass("timestamp").text(formattedDate);
+}
 
-$(document).ready(function() {
-    if (!window.console) window.console = {};
-    if (!window.console.log) window.console.log = function() {};
+function render_message(user, message) {
+    return $("<span>").addClass("message").text('<' + user + '> '+ message);
+}
 
-    $("#messageform").on("submit", function() {
-        newMessage($(this));
-        return false;
-    });
-    $("#messageform").on("keypress", function(e) {
-        if (e.keyCode == 13) {
-            newMessage($(this));
-            return false;
+function render_login(user) {
+    return $("<span>").addClass("notification").text(user + " has logged in");
+}
+
+function render_logout(user) {
+    return $("<span>").addClass("notification").text(user + " has quit");
+}
+
+function render_join(user) {
+    return $("<span>").addClass("notification").text(user + " joined the only chat room");
+}
+
+function render_leave(user) {
+    return $("<span>").addClass("notification").text(user + " left the only chat room");
+}
+
+function render_error(message) {
+    return $("<span>").addClass("error").text(message);
+}
+
+function update_user_list(users) {
+    $("#all_users").empty();
+    var $users = $("<ul>").addClass("nav nav-list");
+    $users.append($("<li>").addClass("col-header").text("Users"));
+    for(var idx in users) {
+        $users.append($("<li>").append(users[idx]));
+    };
+    $("#all_users").append($users);
+}
+
+var P_TYPE = {
+    MESSAGE: 0,
+    REGISTER: 1,
+    LOGIN: 2,
+    LOGOUT: 3,
+    JOIN: 4,
+    LEAVE: 5,
+    LIST: 6,
+    ERROR: -1,
+}
+
+function print_response(jsondata) {
+    var element, $target;
+    if(jsondata['type'] == P_TYPE.MESSAGE) {
+        $target = $('#chat_log');
+        element = render_message(jsondata['user'], jsondata['message']);
+    } else if(jsondata['type'] == P_TYPE.LOGIN) {
+        $target = $('#console_log');
+        element = render_login(jsondata['user']);
+    } else if(jsondata['type'] == P_TYPE.LOGOUT) {
+        $target = $('#console_log');
+        element = render_logout(jsondata['user']);
+    } else if(jsondata['type'] == P_TYPE.JOIN) {
+        $target = $('#chat_log');
+        element = render_join(jsondata['user']);
+    } else if(jsondata['type'] == P_TYPE.LEAVE) {
+        $target = $('#chat_log');
+        element = render_leave(jsondata['user']);
+    } else if(jsondata['type'] == P_TYPE.LIST) {
+        update_user_list(jsondata['users']);
+        return;
+    } else if(jsondata['type'] == P_TYPE.ERROR) {
+        $target = $('#console_log');
+        element = render_error(jsondata['message'])
+    }
+
+    $target.append($("<div>").addClass("row").append(render_date()).append(element));
+}
+
+function print_error(message) {
+    element = render_error(message);
+    $('#console_log').append($("<div>").addClass("row").append(render_date()).append(element));
+}
+
+function send_message(message) {
+    if (message.search("/login") == 0) {
+
+        if (typeof ws !== 'undefined') {
+            // WS is already opened, no way we're allowing to create an another one!
+            print_error("You're already logged in.");
+            return;
         }
-    });
-    $("input#message").select();
-    updater.start();
-});
 
-function newMessage(form) {
-    var message = form.formToDict();
-    if ($("input#message").val().trim().length === 0) {
-        $("input#message").addClass('error');
-        $("input#message").attr('placeholder', 'Please enter a message!')
+        var split = message.split(" ");
+        if(split.length != 2) {
+            return;
+        }
+        var nick = split[1];
+
+        // Create a WS connection
+        ws = new WebSocket("ws://" + location.host + "/chat/login/" + nick);
+
+        ws.onopen = function() {
+            $('#console_log .row:first').remove();  // Hides the helping row
+        };
+
+        ws.onmessage = function(event) { 
+            var jsondata = jQuery.parseJSON(event.data);
+            print_response(jsondata);
+            event.preventDefault();
+        }
+
+        ws.onclose = function() {};
+
         return;
     }
 
-    $("input#message").removeClass('error');
+    if (typeof ws === 'undefined') {
+        print_error("Can't send a message without a connection. Please log in first.");
+        return;
+    }
 
-    updater.socket.send(JSON.stringify(message));
-    $("input#message").val("").select();
+    $('#chat-menu').click();
+    $("#input").focus();
+    return ws.send(generate_payload_message(message));
 }
 
-jQuery.fn.formToDict = function() {
-    var fields = this.serializeArray();
-    var json = {}
-    for (var i = 0; i < fields.length; i++) {
-        json[fields[i].name] = fields[i].value;
-    }
-    if (json.next) delete json.next;
-    return json;
-};
+function generate_payload_message(message) {
+    return JSON.stringify({
+        'type': P_TYPE.MESSAGE,
+        'message': message
+    });
+}
 
-var updater = {
-    socket: null,
+function generate_payload_list() {
+    return JSON.stringify({
+        'type': P_TYPE.LIST
+    });
+}
 
-    start: function() {
-        var url = "ws://" + location.host + "/chatsocket";
-        updater.socket = new WebSocket(url);
-        updater.socket.onmessage = function(event) {
-            updater.showMessage(JSON.parse(event.data));
-        }
-    },
+function generate_payload_login(name) {
+    return JSON.stringify({
+        'type': P_TYPE.LOGIN, 'user': name
+    });
+}
 
-    showMessage: function(message) {
-        var existing = $("#m" + message.id);
-        if (existing.length > 0) return;
-        var node = $(message.html);
-        node.hide();
-        $("#inbox").append(node);
-        node.slideDown();
-    }
-};
+$(function() {
+    $('#input').focus();
+
+    $('form[name=chat]').submit(function(e) {
+        send_message($('#input').val());
+        $('#input').val('');
+        e.preventDefault();
+    });
+
+    $('#input').keypress(function(e) {
+      if (e.which == 13) {
+        $('form[name=chat]').submit();
+        return false;
+      }
+    });
+
+    $(".nav a").click(function(e) {
+       $(".nav .active").removeClass("active");
+       $(this).parent().addClass("active");
+       $('#input').focus();
+    });
+    
+    $("#console-menu").click(function(e) {
+        $("#console-content").show();
+        $("#chat-content").hide();
+    });
+
+    $("#chat-menu").click(function(e) {
+        $("#console-content").hide();
+        $("#chat-content").show();
+    });
+});
--- whirl.py
+++ whirl.py
@@ -1,30 +1,17 @@
-#!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
 import logging
-import os.path
-import uuid
+import os
 
-import tornado.escape
+from collections import defaultdict
+from enum import IntEnum
+
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
 
+from tornado.escape import json_encode, json_decode
 from tornado.options import define, options
+
 
 define("port", default=8667, help="web server's port", type=int)
 define("address", default='127.0.0.1', help="web server's host", type=str)
@@ -32,11 +19,117 @@ define("debug", default=False, help="debug mode", type=bool)
 define("cookie_secret", default=None, help="cookie secret key", type=str)
 
 
+class P_TYPE(IntEnum):
+    """Message/payload types."""
+
+    MESSAGE = 0
+    REGISTER = 1
+    LOGIN = 2
+    LOGOUT = 3
+    JOIN = 4
+    LEAVE = 5
+    LIST = 6
+    ERROR = -1
+
+
+USERS = []  # Stores the active handler instances
+CHANNELS = defaultdict(list)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self, action, nick):
+        USERS.append(self)
+        self.nickname = ''
+        self.logged_in = False
+
+        if action == 'login' and nick is not None:
+            # The user has connected to the websocket via a custom URL.
+            # Let's log the user in automagically right after connect
+            # without having him/her to send an additional LOGIN request
+            self.user_login(nick)
+
+    def on_message(self, message):
+        """Sends a personal/broadcast message based on the command requested."""
+        payload_in = json_decode(message)
+
+        if payload_in['type'] == P_TYPE.LOGIN:
+            self.user_login(payload_in['user'])
+            self.user_list()
+        elif payload_in['type'] == P_TYPE.LOGOUT:
+            USERS.remove(self)
+        elif payload_in['type'] == P_TYPE.JOIN:
+            CHANNELS[payload_in['channel']].append(self.nickname)
+        elif payload_in['type'] == P_TYPE.LEAVE:
+            CHANNELS[payload_in['channel']].remove(self.nickname)
+        elif payload_in['type'] == P_TYPE.MESSAGE:
+            self.broadcast_message(
+                self.generate_payload(
+                    P_TYPE.MESSAGE,
+                    message=payload_in['message'],
+                )
+            )
+        elif payload_in['type'] == P_TYPE.LIST:
+            self.user_list()
+
+    def user_login(self, nick):
+        self.nickname = nick
+        self.logged_in = True
+        self.broadcast_message(self.generate_payload(P_TYPE.LOGIN))
+        self.write_message(
+            self.generate_payload(P_TYPE.LIST, users=[c.nickname for c in USERS])
+        )
+
+    def user_in_channels(self):
+        return [channel for channel, users in CHANNELS.items() if self.nickname in users]
+
+    def user_list(self, broadcast=False):
+        self.write_message(
+            self.generate_payload(P_TYPE.LIST, users=[c.nickname for c in USERS])
+        )
+
+    def broadcast_message(self, payload, except_self=False):
+        """Sends a websocket message to all connected clients.
+except_self being True prevents the message to be sent to its own sender,
+since we don't need to notify the user about him/her being disconnected
+from the server."""
+
+        for c in USERS:
+            if except_self and c == self:
+                continue
+
+            if c.logged_in:
+                c.write_message(payload)
+
+    def generate_payload(self, p_type, **kwargs):
+        """A simple helper function that returns a dictionary
+suitable for the given message type.
+It's ought to be sent to the client right after that."""
+
+        result = {'type': p_type}
+
+        if p_type in (P_TYPE.MESSAGE, P_TYPE.REGISTER, P_TYPE.LOGIN,
+                      P_TYPE.LOGOUT, P_TYPE.JOIN, P_TYPE.LEAVE):
+            result.update({'user': self.nickname})
+
+        result.update(kwargs)
+        return json_encode(result)
+
+    def on_close(self):
+        self.broadcast_message(self.generate_payload(P_TYPE.LOGOUT), except_self=True)
+        USERS.remove(self)
+
+
 class Application(tornado.web.Application):
     def __init__(self, **kwargs):
         handlers = [
             (r"/", MainHandler),
-            (r"/chatsocket", ChatSocketHandler),
+            (r"/chat/(.*)/(.*)", ChatSocketHandler),
+            # e.g.:  /chat/login/vasya - connects successfully and automatically logs the vasya in.
         ]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
@@ -47,58 +140,8 @@ class Application(tornado.web.Application):
         super(Application, self).__init__(handlers, **settings)
 
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("index.html", messages=ChatSocketHandler.cache)
-
-
-class ChatSocketHandler(tornado.websocket.WebSocketHandler):
-    waiters = set()
-    cache = []
-    cache_size = 200
-
-    def get_compression_options(self):
-        # Non-None enables compression with default options.
-        return {}
-
-    def open(self):
-        ChatSocketHandler.waiters.add(self)
-
-    def on_close(self):
-        ChatSocketHandler.waiters.remove(self)
-
-    @classmethod
-    def update_cache(cls, chat):
-        cls.cache.append(chat)
-        if len(cls.cache) > cls.cache_size:
-            cls.cache = cls.cache[-cls.cache_size:]
-
-    @classmethod
-    def send_updates(cls, chat):
-        logging.info("Sending message to %d waiters", len(cls.waiters))
-        for waiter in cls.waiters:
-            try:
-                waiter.write_message(chat)
-            except:
-                logging.error("Error sending message", exc_info=True)
-
-    def on_message(self, message):
-        logging.info("got message %r", message)
-        parsed = tornado.escape.json_decode(message)
-        chat = {
-            "id": str(uuid.uuid4()),
-            "body": parsed["body"],
-        }
-        chat["html"] = tornado.escape.to_basestring(
-            self.render_string("message.html", message=chat)
-        )
-
-        ChatSocketHandler.update_cache(chat)
-        ChatSocketHandler.send_updates(chat)
-
-
 def main():
-    tornado.options.parse_config_file("server.conf")
+    tornado.options.parse_config_file('server.conf')
     app = Application(
         cookie_secret=options.cookie_secret,
         debug=options.debug,
